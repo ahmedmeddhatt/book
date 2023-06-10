859ff0fd1c5aaa72c61f9a0699ec6d34df85fb79
Ahmed Medhat <ahmedmedhat1231@gmail.com>
2023-06-10 21:10:19 +0300

    testing db host

--- config/production/database.js
+++ config/production/database.js
@@ -1,24 +1,24 @@
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.P_DB_EXTERNAL_URL);
-//     module.exports = ({ env }) => ({
-//       connection: {
-//         client: 'postgres',
-//         connection: {
-//           host: process.env.P_DB_HOST,
-//           port: process.env.DB_PORT,
-//           database: process.env.P_DB,
-//           user: process.env.P_DB_USER,
-//           password: process.env.P_DB_PASSWORD,
-//           connectionTimeoutMillis: 3000,
-//           idleTimeoutMillis: 2000,
-//           max: 20,
-//           keepAlive:true,
-//           ssl: true
-//         // connectionString: process.env.P_DB_EXTERNAL_URL,
-//         },
-//         debug: false,
-//       },
-//     });
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.P_DB_EXTERNAL_URL);
+    module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: process.env.P_DB_HOST,
+          port: process.env.DB_PORT,
+          database: process.env.P_DB,
+          user: process.env.P_DB_USER,
+          password: process.env.P_DB_PASSWORD,
+          connectionTimeoutMillis: 3000,
+          idleTimeoutMillis: 2000,
+          max: 20,
+          keepAlive:true,
+          ssl: true
+        // connectionString: process.env.P_DB_EXTERNAL_URL,
+        },
+        debug: false,
+      },
+    });
 
 
 // const parse = require('pg-connection-string').parse;
@@ -55,10 +55,10 @@
 // });
 
 
-const parse = require('pg-connection-string').parse;
-const connectionString = 'postgres://ahmed:9QGgb81XrW0B6XeCSCKJoDEk1DY8d35sU@dpg-cht34tjhp8u4v7sdul5g-a.oregon-postgres.render.com/bms_8vsa?ssl=true';
-const configg = parse(connectionString);
+// const parse = require('pg-connection-string').parse;
+// const connectionString = 'postgres://ahmed:9QGgb81XrW0B6XeCSCKJoDEk1DY8d35sU@dpg-cht34tjhp8u4v7sdul5g-a.oregon-postgres.render.com/bms_8vsa?ssl=true';
+// const configg = parse(connectionString);
 
-module.exports = configg;
+module.exports = config;
 
 
